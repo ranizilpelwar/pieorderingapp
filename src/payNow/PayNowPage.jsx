11bf031baffcbba936d223e4f4c281fd89f60a2f
Rani Zilpelwar <Rani.Zilpelwar@samaritanministries.org>
2021-05-05 14:33:19 -0400

    Add a bit of styling to PayNowPage

--- src/payNow/PayNowPage.jsx
+++ src/payNow/PayNowPage.jsx
@@ -1,3 +1,8 @@
 export const PayNowPage = () => {
-  return <h1>Please pay your outstanding bill before ordering more pies.</h1>;
+  return (
+    <div>
+      <h1>Thank you for your request!</h1>
+      <h3>Please pay your outstanding bill before ordering more pies.</h3>
+    </div>
+  );
 };
