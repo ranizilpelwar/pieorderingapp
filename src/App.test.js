610ee6593ad46817c1e3e24b59788836c75a8795
Rani Zilpelwar <Rani.Zilpelwar@samaritanministries.org>
2021-05-10 22:07:02 -0400

    refactor App test to use it and queryByText to align with new test changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from "./App";
 
 describe("App", () => {
   it('should say hello', () => {
-    render(<App />);
+    const { queryByText } = render(<App />);
   
-    expect(screen.getByText("Hello world!")).toBeInTheDocument();
+    expect(queryByText("Hello world!")).toBeInTheDocument();
   });
 });
