17dc370de0e951b474f2c4f9c39d15948924b906
Rani Zilpelwar <Rani.Zilpelwar@samaritanministries.org>
2021-05-10 22:03:19 -0400

    change test to it

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from "./App";
 
-test('should say hello', () => {
-  render(<App />);
-
-  expect(screen.getByText("Hello world!")).toBeInTheDocument();
-})
+describe("App", () => {
+  it('should say hello', () => {
+    render(<App />);
+  
+    expect(screen.getByText("Hello world!")).toBeInTheDocument();
+  });
+});
